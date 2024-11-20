9ddea6a4dd3a537115667456ec76aca229abdd7b
Benedictekoder <benedictekoder@gmail.com>
2024-11-20 11:34:16 +0100

    js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('Hello!');
+const nameElement = document .querySelector( `#name` )
+const ageElement = document. querySelector(`#age`)
+const heightElement = document. querySelector(`#height`)
+
+const submitButton = document. querySelector(`#submit button`)
+
+function submit ()[
+    const name = nameElement .value
+    const age= ageElement .value
+    const height = heightElement. value
+
+    if(name === `B`){
+        console.log(`navnet er B`)
+    } else if (name ==="B"){
+        console.log("navnet er B")
+     } else {
+        console.log("navnet var ikke D")
+     }
+]
+
+submitButton .addEventListener ("click",submit)
